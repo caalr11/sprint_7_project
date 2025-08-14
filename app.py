3b81cdcb4cdbb951c69b32ac1b638fb6ea4b8995
Carlos Arias <caalr1103@gmail.com>
2025-08-13 21:48:49 -0600

    nueva grafica

--- app.py
+++ app.py
@@ -26,3 +26,20 @@ if hist_button:
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
     st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Constuir diagrama de dispersión')
+if disp_button:
+    st.write(
+        'Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un diagrama de dispersión utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de dispersión
+    fig2 = go.Figure(data=go.Scatter(
+        x=car_data['year'], y=car_data['price'], mode='markers'))
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig2.update_layout(title_text='Relación entre Año y Precio')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig2, use_container_width=True)
